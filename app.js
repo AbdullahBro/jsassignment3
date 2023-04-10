f970e92eb06ca5416c6ca5e5dea86e79c73317a3
Abdullah Nadeem <93202799+AbdullahBro@users.noreply.github.com>
2023-04-10 06:25:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let zakatPercentage = 2.5;
+
+let userInput = +prompt("Please enter your input:");
+console.log(userInput);
+
+let result = prompt(zakatPercentage * userInput);
+console.log(result);
+
+alert("Your zakat value is " + result);
+
+
